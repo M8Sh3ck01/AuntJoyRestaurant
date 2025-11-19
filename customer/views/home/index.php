f942b9709cb22f9cb64f9ba27638e1ae74e011e1
M8Sh3ck01 <misheckchampopa01@gmail.com>
2025-11-19 10:10:19 +0200

    folder structure change

--- customer/views/home/index.php
+++
@@ -1,3 +0,0 @@
-<?php /** @var string $title */ ?>
-<h2><?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8'); ?></h2>
-<p>This is the customer home page rendered through the layout and controller.</p>
